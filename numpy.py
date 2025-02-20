6ad6991a46426cdaef507d7b74616f16f1af7f6b
sahithi jyothsna <48304563+Sahithijyothsna532@users.noreply.github.com>
2025-02-20 22:40:17 +0530

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -17,3 +17,24 @@ a.shape ## defines shape
 a.size  ## defines size 
 a.dtype ## defines type 
 a.ndim ## defines dimension 
+
+#generate arrays
+np.arrange(10) ##generate elements as like range
+np.linspace(0,1,5) #start, end , no of points [0., 0.25, 0.5, 0.75, 1] 
+
+np.ones((3,3)) ##creates all elements as ones in the given dimension 
+np.zeros((3,3)) ##creates all the elements as zeros in the given dimension 
+
+np.eye(3) ## creates diagonal elements with ones in 3d array as in square matrix 
+np.diag([1,2,3,4]) ## creates elemets as diagonal 
+
+np.full((2,3), 7) ##creates 2x3 matrix with all values as 7
+
+np.random.rand(4) ##generate random 4 element arrays 
+
+a = np.arrange(10)
+a.dtype() #gives type of numpy array 
+
+# U can store any data type in the arrary by explicitly defining the data type 
+
+a = np.array([1,2,[1,2], "sahithi", np.array([1,2,3])], dtype = 'O')
